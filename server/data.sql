9fc4c3845546c7a7d68dc07a8c669488cf4e7577
Haythem91 <haylive8@gmail.com>
2024-04-26 13:32:49 +0100

    2nd commit

--- server/data.sql
+++ server/data.sql
@@ -1,2 +1,43 @@
 
 
+CREATE DATABASE threads;
+
+CREATE TABLE users(
+    username VARCHAR(255) PRIMARY KEY,
+    hashed_password VARCHAR(255)
+);
+
+CREATE TABLE profiles(
+    id VARCHAR (255) PRIMARY KEY,
+    username VARCHAR (255),
+    handle VARCHAR (255),
+    bio VARCHAR(255),
+    link VARCHAR (255),
+    img VARCHAR (255),
+    inst_url VARCHAR (255)
+);
+
+CREATE TABLE follows(
+    leaderId VARCHAR (255) PRIMARY KEY,
+    followerId VARCHAR(255)
+
+);
+
+CREATE TABLE threads (
+    id VARCHAR(255) PRIMARY KEY,
+    time_stamp VARCHAR (255),
+    thread_from VARCHAR (255),
+    thread_to VARCHAR (255),
+    reply_to VARCHAR (255),
+    text VARCHAR (255)
+);
+
+CREATE TABLE likes (
+    thread_Id VARCHAR (255) PRIMARY KEY,
+    user_Id VARCHAR (255)
+    );
+
+CREATE TABLE replies (
+    thread_Id VARCHAR (255) PRIMARY KEY,
+    replies VARCHAR (255)
+    );
--- server/index.js
+++ server/index.js
@@ -2,6 +2,31 @@ const PORT = 8000
 const express = require('express')
 const cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(express.json())
+const pool = require('./db')
+
+app.get('/users',async (req,res)=>{
+
+
+    try{
+        const users = await pool.query('SELECT * FROM users;')
+        res.json(users.rows)
+    }catch(err){console.error(err)}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
 
